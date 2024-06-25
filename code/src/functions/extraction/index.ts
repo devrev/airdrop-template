5d563f29feb5c2a41882357e7134a559ad23dadf
navneel mandal <navneel99@gmail.com>
2024-06-25 18:22:05 +0530

    resolve comments

--- code/src/functions/extraction/index.ts
+++ code/src/functions/extraction/index.ts
@@ -1,9 +1,9 @@
-import { AirdropEvent, DemoExtractor as Extractor } from '@devrev/ts-adaas';
+import { AirdropEvent, DemoExtractor } from '@devrev/ts-adaas';
 
 const run = async (events: AirdropEvent[]) => {
   for (const event of events) {
     console.log('Event: ' + JSON.stringify(event));
-    const extractor = new Extractor();
+    const extractor = new DemoExtractor();
     await extractor.run(event);
   }
 };
