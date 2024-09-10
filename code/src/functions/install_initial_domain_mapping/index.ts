55832a5fb7a7f651a7c050ceed07637be20af208
Radovan Jorgić <i-radovan.jorgic@devrev.ai>
2024-09-10 09:53:44 +0200

    Support for new recipe management and example of initial domain mapping

---
+++ code/src/functions/install_initial_domain_mapping/index.ts
@@ -0,0 +1,15 @@
+import { AirdropEvent, installInitialDomainMapping } from '@devrev/ts-adaas';
+
+import initialDomainMapping from '../extraction/initial_domain_mapping.json';
+
+const run = async (events: AirdropEvent[]) => {
+  for (const event of events) {
+    try {
+      await installInitialDomainMapping(event, initialDomainMapping);
+    } catch (error) {
+      console.error('Failed to install initial domain mappings', error);
+    }
+  }
+};
+
+export default run;
